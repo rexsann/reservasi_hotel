2bce59341fbacb1b356332e3bb09bf8a10f04989
Muhammad_Ihsan <m.ihsan2106@gmail.com>
2026-03-04 09:31:46 +0700

    upd

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Silangkan menghubungi kami melalui email di <a href="mailto:info@hotel.com">info@hotel.com</a>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<div>
+   Selamat Datang di Hotel Kami! Kami menyediakan berbagai jenis kamar yang nyaman dan fasilitas terbaik untuk memastikan pengalaman menginap Anda menyenangkan. Apakah Anda mencari kamar dengan pemandangan indah, akses mudah ke pusat kota, atau layanan pelanggan yang luar biasa, kami siap memenuhi kebutuhan Anda. Jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan atau ingin melakukan reservasi. Kami berharap dapat menyambut Anda di hotel kami segera!
+</div>
