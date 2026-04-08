d32c269533507fbf247d3d6fafe394e9bdd96aba
fachaa7 <andifacha956@gmail.com>
2026-04-08 09:14:13 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,16 +25,29 @@
 
     <form method="POST" action="/login" class="space-y-4">
         @csrf
+        
+        <div class="flex flex-col gap-1">
+            <h5 class="text-lg font-bold text-left text-gray-700">Username</h5>
+            <input type="text" name="username" placeholder="Masukkan username"
+                class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400">
+        </div>
 
-        <input type="text" name="username" placeholder="Username"
-            class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400">
-
-        <input type="password" name="password" placeholder="Password"
-            class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400">
+        <div class="flex flex-col gap-1">
+            <h5 class="text-lg font-bold text-left text-gray-700">Password</h5>
+            <input type="password" name="password" placeholder="Masukkan password"
+                class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400">
+        </div>
 
         <button class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600">
             Masuk
         </button>
+
+        <div class="flex justify-between items-center text-sm text-gray-600">
+            <span>Belum punya akun? 
+            <a href="/registrasi" class="text-blue-500 hover:underline">Daftar di sini</a>
+            </span>
+            <a href="/lupapassword" class="text-blue-500 hover:underline">Lupa password?</a>
+        </div>
     </form>
 
 </div>
