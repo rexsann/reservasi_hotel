ea6a777427200e49e72e4652a2ab812330053538
Muhammad_Ihsan <m.ihsan2106@gmail.com>
2026-04-24 01:47:12 +0700

    update halaman layout

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
@@ -20,5 +21,16 @@
 
     @include('components.footer')
 
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            flatpickr("#dateRange", {
+                mode: "range",
+                dateFormat: "d M Y",
+                minDate: "today"
+            });
+        });
+    </script>   
+
 </body>
+
 </html>
