a32492ff57f0f5e271f8afea0b7a561d17eefe48
Muhammad_Ihsan <m.ihsan2106@gmail.com>
2026-04-25 17:01:38 +0700

    update halaman home

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+{
+    $data = [
+        'id' => 1,
+        'produk' => 'Laptop Gaming'
+    ];
+
+    return view('list_product', compact('data'));
+}
+}
