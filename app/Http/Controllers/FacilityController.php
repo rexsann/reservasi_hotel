a2715b5e7c627c8bef7d010563e178ddf0ceefbb
diar_dina19 <diardina1009@gmail.com>
2026-05-27 19:24:21 +0700

    update admin

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -3,11 +3,43 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Room;
+use App\Models\Facility;
 
 class FacilityController extends Controller
 {
     public function index()
-    {
-        return view('admin.facility');
-    }
+{
+    $rooms = Room::select('type')
+        ->distinct()
+        ->get();
+
+    $facilities = Facility::all();
+
+    return view('admin.facility', compact('rooms', 'facilities'));
+}
+
+public function store(Request $request)
+{
+    $request->validate([
+        'type' => 'required',
+        'name' => 'required'
+    ]);
+
+    Facility::create([
+        'room_type' => $request->type,
+        'name'      => $request->name
+    ]);
+
+    return redirect('/admin/facility');
+}
+
+public function destroy($id)
+{
+    Facility::findOrFail($id)->delete();
+
+    return redirect('/admin/facility');
+}
 }
