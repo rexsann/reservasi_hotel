b7c2101f53213bb8acccfca3a77d9e9f45d092b1
diar_dina19 <diardina1009@gmail.com>
2026-05-27 19:24:35 +0700

    update admin

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Offer;
 
 class OfferController extends Controller
 {
     public function index()
     {
-        return view('admin.offers');
+        $offers = Offer::all();
+
+        return view('admin.offers', compact('offers'));
+    }
+
+    public function store(Request $request)
+    {
+        Offer::create([
+            'name' => $request->name,
+            'room_type' => $request->room_type,
+            'price' => $request->price,
+            'benefits' => json_encode($request->benefits),
+        ]);
+
+        return redirect()->back();
+    }
+
+    public function destroy($id)
+    {
+        Offer::findOrFail($id)->delete();
+
+        return redirect()->back();
     }
 }
