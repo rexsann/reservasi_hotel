2c76bb2f59fdc318918a32035e92fbd8bfaf1db2
Muhammad_Ihsan <m.ihsan2106@gmail.com>
2026-05-29 11:02:46 +0700

    update Model

---
+++ app/Models/Payment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'reservation_id',
+        'payment_method',
+        'proof_image',
+        'amount',
+        'status',
+        'paid_at'
+    ];
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+}
