ef150ec75a1022ecef67d76a76f480b789677c91
diar_dina19 <diardina1009@gmail.com>
2026-05-30 18:33:14 +0700

    update admin

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -4,19 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Reservation;
-
+use Illuminate\Http\Request;
 class UserManagementController extends Controller
 {
-    public function index()
-    {
-        $users = User::all();
+   public function index(Request $request)
+{
+    $search = $request->search;
 
-        foreach ($users as $user)
-        {
-            $user->total_reservations =
-                Reservation::where('email', $user->email)->count();
-        }
+    $users = User::query()
 
-        return view('admin.user_management', compact('users'));
-    }
+        ->when($search, function ($query) use ($search) {
+
+            $query->where('name', 'like', '%' . $search . '%')
+                  ->orWhere('email', 'like', '%' . $search . '%');
+
+        })
+
+        ->latest()
+        ->get();
+
+    return view('admin.user_management', compact('users'));
+}
 }
