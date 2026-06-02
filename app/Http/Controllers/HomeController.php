2383c5333a9c507f00d1a2666e1202479441e9f8
Muhammad_Ihsan <m.ihsan2106@gmail.com>
2026-06-02 16:23:29 +0700

    update Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,28 +10,28 @@ use App\Models\Reservation;
 
 class HomeController extends Controller
 {
-   public function index(Request $request)
-{
-    $checkIn  = $request->check_in;
-    $checkOut = $request->check_out;
+    public function index(Request $request)
+    {
+        $checkIn  = $request->check_in;
+        $checkOut = $request->check_out;
 
-    // Cari room_type yang sudah terisi di rentang tanggal
-    $bookedTypes = [];
+        // Cari room_type yang sudah terisi di rentang tanggal
+        $bookedTypes = [];
 
-    if ($checkIn && $checkOut) {
-        $bookedTypes = Reservation::whereIn('status', ['confirmed', 'pending'])
-            ->where(function ($q) use ($checkIn, $checkOut) {
-                $q->where('check_in', '<', $checkOut)
-                  ->where('check_out', '>', $checkIn);
-            })
-            ->pluck('room_type')
-            ->toArray();
+        if ($checkIn && $checkOut) {
+            $bookedTypes = Reservation::whereIn('status', ['confirmed', 'pending'])
+                ->where(function ($q) use ($checkIn, $checkOut) {
+                    $q->where('check_in', '<', $checkOut)
+                        ->where('check_out', '>', $checkIn);
+                })
+                ->pluck('room_type')
+                ->toArray();
+        }
+
+        $offers     = Offer::all();
+        $facilities = Facility::all();
+        $types = Offer::distinct()->pluck('room_type'); // ambil dari DB
+
+        return view('pages.home', compact('offers', 'facilities', 'bookedTypes', 'checkIn', 'checkOut', 'types'));
     }
-
-    $offers     = Offer::all();
-    $facilities = Facility::all();
-
-    return view('pages.home', compact('offers', 'facilities', 'bookedTypes', 'checkIn', 'checkOut'));
-}
-    
 }
