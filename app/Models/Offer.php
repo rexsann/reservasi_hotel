60d5e221aec517c792f53fe16818df25d10ad580
diar_dina19 <diardina1009@gmail.com>
2026-06-11 15:34:50 +0700

    upd admin

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -8,8 +8,18 @@ class Offer extends Model
 {
     protected $fillable = [
         'name',
-        'room_type',
+        'room_type_id',
         'price',
         'benefits'
     ];
+
+    public function roomType()
+    {
+        return $this->belongsTo(RoomType::class);
+    }
+
+    public function rooms()
+    {
+        return $this->hasMany(Room::class);
+    }
 }
