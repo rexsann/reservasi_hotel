eb3b665e7ffd3e6c439c1e8a440240b05617afed
diar_dina19 <diardina1009@gmail.com>
2026-06-11 15:34:40 +0700

    upd admin

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -10,9 +10,18 @@ class Room extends Model
 
     protected $fillable = [
         'room_name',
-        'type',
-        'offer',
-        'price_per_night',
-        'status'
+        'room_type_id',
+        'offer_id',
+        'status',
     ];
+
+    public function roomType()
+    {
+        return $this->belongsTo(RoomType::class);
+    }
+
+    public function offer()
+    {
+        return $this->belongsTo(Offer::class);
+    }
 }
