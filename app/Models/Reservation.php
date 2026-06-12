5c7e3739e569af524164f67d07df8b96b87adf05
Muhammad_Ihsan <m.ihsan2106@gmail.com>
2026-06-12 16:20:15 +0700

    update js

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -9,24 +9,24 @@ class Reservation extends Model
     protected $table = 'reservation';
 
     protected $fillable = [
-    'reservation_code',
-    'invoice_code',
-    'name',
-    'email',
-    'room_id',
-    'room_name',
-    'room_type',
-    'offer',
-    'check_in',
-    'check_out',
-    'guest_total',
-    'total_price',
-    'status',
-    'paid_at',
-    'checked_in_at',
-    'checked_out_at',
-    'cancelled_at',
-];
+        'reservation_code',
+        'invoice_code',
+        'name',
+        'email',
+        'room_id',
+        'room_name',
+        'room_type_id',
+        'offer_id',
+        'check_in',
+        'check_out',
+        'guest_total',
+        'total_price',
+        'status',
+        'paid_at',
+        'checked_in_at',
+        'checked_out_at',
+        'cancelled_at',
+    ];
 
     protected static function boot()
     {
@@ -38,7 +38,25 @@ class Reservation extends Model
     }
 
     public function payments()
-{
-    return $this->hasMany(Payment::class);
-}
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    // RELASI ROOM TYPE
+    public function roomType()
+    {
+        return $this->belongsTo(RoomType::class, 'room_type_id');
+    }
+
+    // RELASI OFFER
+    public function offer()
+    {
+        return $this->belongsTo(Offer::class, 'offer_id');
+    }
+
+    // RELASI ROOM
+    public function room()
+    {
+        return $this->belongsTo(Room::class, 'room_id');
+    }
 }
