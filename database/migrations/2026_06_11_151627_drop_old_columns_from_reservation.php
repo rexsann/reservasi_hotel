e7824e2b6b1d41e0fbd927dc473aec58adbafc48
Muhammad_Ihsan <m.ihsan2106@gmail.com>
2026-06-12 10:22:57 +0700

    update Migration

---
+++ database/migrations/2026_06_11_151627_drop_old_columns_from_reservation.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::table('reservation', function (Blueprint $table) {
+        $table->dropColumn([
+            'room_name',
+            'room_type',
+            'offer'
+        ]);
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reservation', function (Blueprint $table) {
+            //
+        });
+    }
+};
