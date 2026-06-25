42045db13232b908d714fdbdf8c0ed68cac9be8e
Muhammad_Ihsan <m.ihsan2106@gmail.com>
2026-06-25 15:07:32 +0700

    update Blade

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -6,14 +6,14 @@ Halo, **{{ $pembayaran->reservation->name }}**!
 Pembayaran Anda telah **dikonfirmasi**. Berikut detail invoice Anda:
 
 <x-mail::panel>
-**No. Reservasi:** {{ $pembayaran->reservation->reservation_code }}
-**Nama Tamu:** {{ $pembayaran->reservation->name }}
-**Kamar:** {{ $pembayaran->reservation->room_name }}
-**Check-in:** {{ \Carbon\Carbon::parse($pembayaran->reservation->check_in)->format('d M Y') }}
-**Check-out:** {{ \Carbon\Carbon::parse($pembayaran->reservation->check_out)->format('d M Y') }}
-**Metode Pembayaran:** {{ $pembayaran->payment_method }}
-**Total Dibayar:** Rp {{ number_format($pembayaran->amount, 0, ',', '.') }}
-**Status:** ✅ Paid
+<br>**No. Reservasi:** {{ $pembayaran->reservation->reservation_code }}</br>
+<br>**Nama Tamu:** {{ $pembayaran->reservation->name }}</br>
+<br>**Kamar:** {{ $pembayaran->reservation->room_name }}</br>
+<br>**Check-in:** {{ \Carbon\Carbon::parse($pembayaran->reservation->check_in)->format('d M Y') }}</br>
+<br>**Check-out:** {{ \Carbon\Carbon::parse($pembayaran->reservation->check_out)->format('d M Y') }}</br>
+<br>**Metode Pembayaran:** {{ $pembayaran->payment_method }}</br>
+<br>**Total Dibayar:** Rp {{ number_format($pembayaran->amount, 0, ',', '.') }}</br>
+<br>**Status:** ✅ Paid
 </x-mail::panel>
 
 Terima kasih telah melakukan reservasi. Kami menantikan kedatangan Anda!
